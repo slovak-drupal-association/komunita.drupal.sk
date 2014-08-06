f6cc8ff8e257ee47162d4b4f40092e96aa5ecbda
Juraj Chlebec <havran@gmail.com>
2014-08-06 21:39:01 +0200

    fix number of parameters in process() method

--- docroot/modules/texy/src/Plugin/Filter/FilterTexy.php
+++ docroot/modules/texy/src/Plugin/Filter/FilterTexy.php
@@ -9,13 +9,15 @@ namespace Drupal\texy\Plugin\Filter;
 use Drupal\filter\Plugin\FilterBase;
 
 /** 
- * Provides a filter to display any HTML as plain text.
- *
+ * Provides a filter to display TEXY as HTMLt.
+ * 
  * @Filter(
  *   id = "filter_texy",
- *   module = "texy",
  *   title = @Translation("Texy! filter"),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *   settings = {
+ *   }
+ *   module = "texy",
  *   weight = -10
  * )
  */
@@ -24,7 +26,7 @@ class FilterTexy extends FilterBase {
   /** 
    * {@inheritdoc}
    */
-  public function process($text, $langcode, $cache, $cache_id) {
+  public function process($text, $langcode) {
     $text = str_replace('foo', 'bar', $text);
 
     return $text;
