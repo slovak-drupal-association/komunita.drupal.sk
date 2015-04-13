20dbb4f7c67384a1ea901eb6d660b00c400075d1
Acquia Cloud <support@acquia.com>
2015-04-13 12:55:30 +0000

    Importing site archive

--- docroot/index.php
+++ docroot/index.php
@@ -1,39 +1,45 @@
 <?php
-// $Id: index.php,v 1.94 2007/12/26 08:46:48 dries Exp $
 
 /**
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
  *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
  * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
  */
 
-require_once './includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+use Drupal\Core\DrupalKernel;
+use Drupal\Core\Site\Settings;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-$return = menu_execute_active_handler();
+$autoloader = require_once 'autoload.php';
 
-// Menu status constants are integers; page content is a string.
-if (is_int($return)) {
-  switch ($return) {
-    case MENU_NOT_FOUND:
-      drupal_not_found();
-      break;
-    case MENU_ACCESS_DENIED:
-      drupal_access_denied();
-      break;
-    case MENU_SITE_OFFLINE:
-      drupal_site_offline();
-      break;
+try {
+
+  $request = Request::createFromGlobals();
+  $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
+  $response = $kernel
+      ->handle($request)
+      // Handle the response object.
+      ->prepare($request)->send();
+  $kernel->terminate($request, $response);
+}
+catch (HttpExceptionInterface $e) {
+  $response = new Response($e->getMessage(), $e->getStatusCode());
+  $response->prepare($request)->send();
+}
+catch (Exception $e) {
+  $message = 'If you have just changed code (for example deployed a new module or moved an existing one) read <a href="http://drupal.org/documentation/rebuild">http://drupal.org/documentation/rebuild</a>';
+  if (Settings::get('rebuild_access', FALSE)) {
+    $rebuild_path = $GLOBALS['base_url'] . '/rebuild.php';
+    $message .= " or run the <a href=\"$rebuild_path\">rebuild script</a>";
   }
-}
-elseif (isset($return)) {
-  // Print any value (including an empty string) except NULL or undefined:
-  print theme('page', $return);
-}
 
-drupal_page_footer();
+  // Set the response code manually. Otherwise, this response will default to a
+  // 200.
+  http_response_code(500);
+  print $message;
+  throw $e;
+}
