e00f4887ef3ae2e8c68bd57b241a36723fc65468
root <root@localhost.localdomain>
2015-08-26 20:34:08 +0200

    Issue #8 Ignore files directory

---
+++ docroot/drupal/sites/default/files/php/twig/e4e0fe30_block--system-messages-block.html.twig_057ea7e0d59f3b7574f7a29dfd651954fb95aa11eedd0a358ec0ccee6419679f/37e554f5bd213c67e7f7254df0fc6b1691ac9d92328d5931ed6e907e40772fea.php
@@ -0,0 +1,38 @@
+<?php
+
+/* core/modules/system/templates/block--system-messages-block.html.twig */
+class __TwigTemplate_057ea7e0d59f3b7574f7a29dfd651954fb95aa11eedd0a358ec0ccee6419679f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 15
+        echo $this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/system/templates/block--system-messages-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 15,);
+    }
+}
