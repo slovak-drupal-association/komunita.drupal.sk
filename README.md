f25fed4ca435221019ef253008e47aba71dfea87
David Lukac <david.lukac@gmail.com>
2015-10-14 22:08:24 +0200

    Issue #36 - Added installation documentation and `reinstall.sh` script.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Working with this repository
 
 - we use GitFlow as branching strategy
+
+## Installation
+
+Use install script located in `scripts/reinstall.sh`.
--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}/../docroot/sites/default
+
+drush si --locale=sk --site-name="Drupal Slovensko" --site-mail="info@drupal.sk" -y
