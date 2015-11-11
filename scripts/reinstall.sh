18fedaff8527d9c368d3adb35b6bb4723232015a
m1n0 <mino.lukac@gmail.com>
2015-11-11 20:58:07 +0100

    Issue #36 Updating install script

--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}/../docroot/sites/default
 
-drush si --locale=sk --site-name="Drupal Slovensko" --site-mail="info@drupal.sk" -y
+drush si --uri=dsk.dd:8083 -y
